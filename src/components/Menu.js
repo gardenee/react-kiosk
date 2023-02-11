fc646f4eee1497753181cd516145346eade62dd8
gardenee <snnjjang@naver.com>
2023-02-12 00:16:51 +0900

    menu-tab onClick event

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -14,13 +14,22 @@ const Menu = () => {
 	const [activeTab, setActiveTab] = useState("menu-tab-1");
 
 	useEffect(() => {
+		console.log(activeTab);
 		document.getElementById(activeTab).classList.add("active");
 	}, [activeTab]);
+
+	const handleClick = (event) => {
+		document.getElementById(activeTab).classList.remove("active");
+		setActiveTab(event.target.id);
+	};
 
 	return (
 		<ul className="menu-tab">
 			{menuList.map((item, index) => (
-				<li key={index + 1} id={`menu-tab-${index + 1}`}>
+				<li
+					key={index + 1}
+					id={`menu-tab-${index + 1}`}
+					onClick={(e) => handleClick(e)}>
 					{item}
 				</li>
 			))}
